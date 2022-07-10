27ac6a9d401dc32c4a938bee2a9ff9e01321677d
vishwanathevs <vishwanathevs@outlook.com>
2022-07-09 20:07:03 -0500

    Exception handling for possible dup of sub ids

--- scraper_manage/scraper.py
+++ scraper_manage/scraper.py
@@ -3,6 +3,7 @@ import praw
 import pandas as pd
 import datetime
 from datetime import datetime as dt
+import sqlite3
 
 def init_scraper(client_id, client_secret, user_agent):
 
@@ -11,6 +12,28 @@ def init_scraper(client_id, client_secret, user_agent):
                               user_agent=user_agent)
     
     return scraper_obj
+
+def validate_data_entry(new_submission_admin, db_connection, admin_recs_tname, 
+                        subreddit):
+
+    # GETTING EXISTING SUBMISSIONS
+    admin_recs_query = """SELECT * FROM {} 
+                          WHERE subreddit =\"{}\"""".format(admin_recs_tname, subreddit)
+    admin_recs_table = pd.read_sql_query(admin_recs_query, db_connection)
+
+    # GETTING SUBMISSIONS WITH COMMON IDS
+    admin_recs_ids = admin_recs_table.sub_id
+    new_submission_ids = new_submission_admin.sub_id
+    common_submission_ids = [id for id in new_submission_ids 
+                                if id in list(admin_recs_ids)]
+    columns_to_verify = ["sub_id", "sub_title", "ts_sub_created"]
+
+    print("Existing admin records with conflicting IDs:")
+    print(admin_recs_table.loc[admin_recs_table.sub_id.isin(common_submission_ids), columns_to_verify])
+    print("New admin records with conflicting IDs:")
+    print(new_submission_admin.loc[new_submission_admin.sub_id.isin(common_submission_ids), columns_to_verify])
+
+    return common_submission_ids
 
 def proc_new_submissions(subs, existing_sub_ids, subs_subreddit, title_keywords, 
                          db_connection, admin_recs_tname, sub_info_tname, 
@@ -43,10 +66,31 @@ def proc_new_submissions(subs, existing_sub_ids, subs_subreddit, title_keywords,
     new_sub_info = new_sub_info_df[["sub_id", "ts_last_polled", "num_ups", "up_ratio", "num_comms", "is_sub_locked"]]
 
     # WRITING TABLES
-    new_sub_admininfo.to_sql(name=admin_recs_tname, con=db_connection, 
-                             if_exists="append", index=False)
-    new_sub_info.to_sql(name=sub_info_tname, con=db_connection, 
-                        if_exists="append", index=False)
+    try:
+        new_sub_admininfo.to_sql(name=admin_recs_tname, con=db_connection, 
+                                 if_exists="append", index=False)
+        new_sub_info.to_sql(name=sub_info_tname, con=db_connection, 
+                            if_exists="append", index=False)
+    except sqlite3.IntegrityError as ex:
+        message = """SQL Integrity Error {0} when processing new submissions. Arguments:\n{1!r}"""
+        
+        if ex.args[0] == "UNIQUE constraint failed: submission_admin.sub_id":
+            conflicting_ids = validate_data_entry(new_submission_admin=new_sub_admininfo, 
+                                                  db_connection=db_connection, 
+                                                  admin_recs_tname=admin_recs_tname, 
+                                                  subreddit=subs_subreddit)
+            
+            new_sub_admininfo_wo_conflict = new_sub_admininfo.loc[~new_sub_admininfo.sub_id.isin(conflicting_ids), :]
+            new_sub_info_wo_conflict = new_sub_info.loc[~new_sub_info.sub_id.isin(conflicting_ids), :]
+            
+            if len(new_sub_admininfo_wo_conflict.index.values) == 0:
+                print("No new submissions after correcting for submission ID conflicts at {} in subreddit {}".format(poll_datetime, subs_subreddit))
+                return None
+            
+            new_sub_admininfo_wo_conflict.to_sql(name=admin_recs_tname, con=db_connection, 
+                                                 if_exists="append", index=False)
+            new_sub_info_wo_conflict.to_sql(name=sub_info_tname, con=db_connection, 
+                                            if_exists="append", index=False)
 
     return None
 
